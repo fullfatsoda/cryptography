e6f55852e96bfa60953ef078f6942d2d469e03ff
Christopher <65589469+fullfatsoda@users.noreply.github.com>
2020-06-14 09:55:25 +0100

    Add files via upload

---
+++ caesar_decrypt.py
@@ -0,0 +1,33 @@
+message = "Qi'ohmvj nhmx'obgi Duivmiv S'mzfi xhbi'obhm giubho"
+
+def decrypt(message, key):
+    key = -key
+    decrypted_message = ""      # decrypted message to return
+    for symbol in message:      # for every character in the message
+        if symbol.isalpha():    # if the character is alaphabetical
+            num = ord(symbol)   # set numerical value to character
+            num += key          # change it by the key number
+
+            # 26 is the highest value in the alphabet depending on the key
+            # num needs to 'wrap back around' to the start of the alphabet
+            if symbol.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif symbol.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+
+            # add symbol to decrypted_message
+            decrypted_message += chr(num)
+        else:
+            # symbol is not alphabetic
+            # add it to decrypted_message
+            decrypted_message += symbol
+
+    return decrypted_message
+
+print(decrypt(message, 1))
---
+++ caesar_encrypt.py
@@ -0,0 +1,32 @@
+message = "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn"
+
+def encrypt(message, key):
+    encrypted_message = ""      # encrypted message to return
+    for symbol in message:      # for every character in the message
+        if symbol.isalpha():    # if the character is alaphabetical
+            num = ord(symbol)   # set numerical value to character
+            num += key          # change it by the key number
+
+            # 26 is the highest value in the alphabet depending on the key
+            # num needs to 'wrap back around' to the start of the alphabet
+            if symbol.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif symbol.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+
+            # add ciphered symbol to encrypted_message
+            encrypted_message += chr(num)
+        else:
+            # symbol is not alphabetic
+            # add it to encrypted_message
+            encrypted_message += symbol
+
+    return encrypted_message
+
+print(encrypt(message, 1))
